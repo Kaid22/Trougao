08b21464b522b0cf994a2d437431576350c74394
Kaid22 <avdickaid@gmail.com>
2021-02-18 11:42:07 +0100

    kommita

--- Trougao/Form1.cs
+++ Trougao/Form1.cs
@@ -16,5 +16,34 @@ namespace Trougao
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+        Random r = new Random();
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                Point o = new Point(e.X, e.Y);
+                Trougao t = new Trougao(o);
+                t.A = Convert.ToInt32(textBox1.Text);
+                Graphics g = CreateGraphics();
+                Color boja = Color.FromArgb(160, r.Next(256), r.Next(256), r.Next(256));
+                t.Boji(g, boja);
+                t.Crtaj(g, Color.Black);
+                textBox2.Text = t.Acm.ToString("0.00");
+                double obim = 3 * t.Acm;
+                textBox3.Text = obim.ToString("0.00");
+            }
+            catch (Exception izuzetak)
+            {
+
+                MessageBox.Show(izuzetak.Message, "GRESKA");
+            }
+        }
     }
 }
